b92f0f53e61d4ecd7bf1db37240e44c7cd503fc6
unknown <okorojiebube2@gmail.com>
2021-08-01 22:39:11 -0600

    made changes to index.js file

--- src/index.js
+++ src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-	<>
-	<form name='contact' method='post'>
+	<React.StrictMode>
+		<form name='contact' method='post'>
 	<input type='text' name='name' />
 	<input type='email' name='email' />
 	<textarea name='message' />
 	<button type='submit'> Send Message </button>
 	</form>
-	<React.StrictMode>
-				<App />
+	<App />
 	</React.StrictMode>,
 	document.getElementById('root')
-	</>
 );
 
